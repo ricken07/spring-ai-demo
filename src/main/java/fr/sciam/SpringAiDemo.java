97e30fcc38fe9a97510f2890579437bde095c65e
Ricken Bazolo <ricken.bazolo@gmail.com>
2024-03-19 12:05:17 +0100

    first commit

---
+++ src/main/java/fr/sciam/SpringAiDemo.java
@@ -0,0 +1,11 @@
+package fr.sciam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiDemo.class, args);
+    }
+}
