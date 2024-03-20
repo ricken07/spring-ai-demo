b5f3d5ff4981e3376306eecdd5795ad2c14bd141
Ricken Bazolo <ricken.bazolo@gmail.com>
2024-03-20 19:53:06 +0100

    adding hilla ui

--- src/main/java/fr/sciam/SpringAiDemo.java
+++ src/main/java/fr/sciam/SpringAiDemo.java
@@ -1,10 +1,13 @@
 package fr.sciam;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAiDemo {
+@Theme(value = "spring-ai-demo")
+public class SpringAiDemo implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(SpringAiDemo.class, args);
     }
